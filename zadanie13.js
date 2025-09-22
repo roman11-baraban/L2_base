b8dcc5a7b9889bf77c76fd061c147cc7663e736c
Alshanets Roman <0330roman@gmail.com>
2025-09-22 19:16:04 +0300

    добавил 13-ое задание

---
+++ zadanie13.js
@@ -0,0 +1,7 @@
+function hexStringToRGB(hex) {
+  hex = hex.toUpperCase().replace('#', '');
+  let r = parseInt(hex.slice(0, 2), 16);
+  let g = parseInt(hex.slice(2, 4), 16);
+  let b = parseInt(hex.slice(4, 6), 16);
+  return { r: r, g: g, b: b };
+}
